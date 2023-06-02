38ebbf5a2fd9152e7ce22be1724365eba4f53078
Nijat Muradov <live4ever4@gmail.com>
2023-06-02 11:53:31 +0400

    Add view

--- DesktopCalculator/Form1.Designer.cs
+++ DesktopCalculator/Form1.Designer.cs
@@ -28,12 +28,282 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            zeroButton = new Button();
+            pointButton = new Button();
+            clearButton = new Button();
+            addButton = new Button();
+            subButton = new Button();
+            threeButton = new Button();
+            twoButton = new Button();
+            oneButton = new Button();
+            multButton = new Button();
+            sixButton = new Button();
+            fiveButton = new Button();
+            fourButton = new Button();
+            divButton = new Button();
+            nineButton = new Button();
+            eightButton = new Button();
+            sevenButton = new Button();
+            equalMarkButton = new Button();
+            inputRichTextBox = new RichTextBox();
+            solveRichTextBox = new RichTextBox();
+            SuspendLayout();
+            // 
+            // zeroButton
+            // 
+            zeroButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            zeroButton.Location = new Point(12, 307);
+            zeroButton.Name = "zeroButton";
+            zeroButton.Size = new Size(59, 51);
+            zeroButton.TabIndex = 0;
+            zeroButton.Text = "0";
+            zeroButton.UseVisualStyleBackColor = true;
+            // 
+            // pointButton
+            // 
+            pointButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            pointButton.Location = new Point(77, 307);
+            pointButton.Name = "pointButton";
+            pointButton.Size = new Size(59, 51);
+            pointButton.TabIndex = 1;
+            pointButton.Text = ".";
+            pointButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            clearButton.BackColor = Color.Tomato;
+            clearButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            clearButton.Location = new Point(142, 307);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(59, 51);
+            clearButton.TabIndex = 2;
+            clearButton.Text = "C";
+            clearButton.UseVisualStyleBackColor = false;
+            // 
+            // addButton
+            // 
+            addButton.BackColor = Color.Turquoise;
+            addButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            addButton.Location = new Point(207, 307);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(59, 51);
+            addButton.TabIndex = 3;
+            addButton.Text = "+";
+            addButton.UseVisualStyleBackColor = false;
+            // 
+            // subButton
+            // 
+            subButton.BackColor = Color.Turquoise;
+            subButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            subButton.Location = new Point(207, 250);
+            subButton.Name = "subButton";
+            subButton.Size = new Size(59, 51);
+            subButton.TabIndex = 7;
+            subButton.Text = "-";
+            subButton.UseVisualStyleBackColor = false;
+            // 
+            // threeButton
+            // 
+            threeButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            threeButton.Location = new Point(142, 250);
+            threeButton.Name = "threeButton";
+            threeButton.Size = new Size(59, 51);
+            threeButton.TabIndex = 6;
+            threeButton.Text = "3";
+            threeButton.UseVisualStyleBackColor = true;
+            // 
+            // twoButton
+            // 
+            twoButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            twoButton.Location = new Point(77, 250);
+            twoButton.Name = "twoButton";
+            twoButton.Size = new Size(59, 51);
+            twoButton.TabIndex = 5;
+            twoButton.Text = "2";
+            twoButton.UseVisualStyleBackColor = true;
+            // 
+            // oneButton
+            // 
+            oneButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            oneButton.Location = new Point(12, 250);
+            oneButton.Name = "oneButton";
+            oneButton.Size = new Size(59, 51);
+            oneButton.TabIndex = 4;
+            oneButton.Text = "1";
+            oneButton.UseVisualStyleBackColor = true;
+            // 
+            // multButton
+            // 
+            multButton.BackColor = Color.Turquoise;
+            multButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            multButton.Location = new Point(208, 193);
+            multButton.Name = "multButton";
+            multButton.Size = new Size(59, 51);
+            multButton.TabIndex = 11;
+            multButton.Text = "*";
+            multButton.UseVisualStyleBackColor = false;
+            // 
+            // sixButton
+            // 
+            sixButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            sixButton.Location = new Point(143, 193);
+            sixButton.Name = "sixButton";
+            sixButton.Size = new Size(59, 51);
+            sixButton.TabIndex = 10;
+            sixButton.Text = "6";
+            sixButton.UseVisualStyleBackColor = true;
+            // 
+            // fiveButton
+            // 
+            fiveButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            fiveButton.Location = new Point(78, 193);
+            fiveButton.Name = "fiveButton";
+            fiveButton.Size = new Size(59, 51);
+            fiveButton.TabIndex = 9;
+            fiveButton.Text = "5";
+            fiveButton.UseVisualStyleBackColor = true;
+            // 
+            // fourButton
+            // 
+            fourButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            fourButton.Location = new Point(13, 193);
+            fourButton.Name = "fourButton";
+            fourButton.Size = new Size(59, 51);
+            fourButton.TabIndex = 8;
+            fourButton.Text = "4";
+            fourButton.UseVisualStyleBackColor = true;
+            // 
+            // divButton
+            // 
+            divButton.BackColor = Color.Turquoise;
+            divButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            divButton.Location = new Point(208, 136);
+            divButton.Name = "divButton";
+            divButton.Size = new Size(59, 51);
+            divButton.TabIndex = 15;
+            divButton.Text = "/";
+            divButton.UseVisualStyleBackColor = false;
+            // 
+            // nineButton
+            // 
+            nineButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            nineButton.Location = new Point(143, 136);
+            nineButton.Name = "nineButton";
+            nineButton.Size = new Size(59, 51);
+            nineButton.TabIndex = 14;
+            nineButton.Text = "9";
+            nineButton.UseVisualStyleBackColor = true;
+            // 
+            // eightButton
+            // 
+            eightButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            eightButton.Location = new Point(78, 136);
+            eightButton.Name = "eightButton";
+            eightButton.Size = new Size(59, 51);
+            eightButton.TabIndex = 13;
+            eightButton.Text = "8";
+            eightButton.UseVisualStyleBackColor = true;
+            // 
+            // sevenButton
+            // 
+            sevenButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            sevenButton.Location = new Point(13, 136);
+            sevenButton.Name = "sevenButton";
+            sevenButton.Size = new Size(59, 51);
+            sevenButton.TabIndex = 12;
+            sevenButton.Text = "7";
+            sevenButton.UseVisualStyleBackColor = true;
+            // 
+            // equalMarkButton
+            // 
+            equalMarkButton.BackColor = SystemColors.Highlight;
+            equalMarkButton.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            equalMarkButton.Location = new Point(13, 364);
+            equalMarkButton.Name = "equalMarkButton";
+            equalMarkButton.Size = new Size(253, 51);
+            equalMarkButton.TabIndex = 16;
+            equalMarkButton.Text = "=";
+            equalMarkButton.UseVisualStyleBackColor = false;
+            // 
+            // inputRichTextBox
+            // 
+            inputRichTextBox.BorderStyle = BorderStyle.FixedSingle;
+            inputRichTextBox.Enabled = false;
+            inputRichTextBox.Font = new Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            inputRichTextBox.ForeColor = SystemColors.ControlText;
+            inputRichTextBox.Location = new Point(13, 44);
+            inputRichTextBox.Name = "inputRichTextBox";
+            inputRichTextBox.ReadOnly = true;
+            inputRichTextBox.RightToLeft = RightToLeft.Yes;
+            inputRichTextBox.Size = new Size(255, 86);
+            inputRichTextBox.TabIndex = 17;
+            inputRichTextBox.Text = "";
+            // 
+            // solveRichTextBox
+            // 
+            solveRichTextBox.BorderStyle = BorderStyle.FixedSingle;
+            solveRichTextBox.Enabled = false;
+            solveRichTextBox.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            solveRichTextBox.Location = new Point(13, 6);
+            solveRichTextBox.Name = "solveRichTextBox";
+            solveRichTextBox.ReadOnly = true;
+            solveRichTextBox.RightToLeft = RightToLeft.Yes;
+            solveRichTextBox.Size = new Size(255, 32);
+            solveRichTextBox.TabIndex = 18;
+            solveRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(280, 427);
+            Controls.Add(solveRichTextBox);
+            Controls.Add(inputRichTextBox);
+            Controls.Add(equalMarkButton);
+            Controls.Add(divButton);
+            Controls.Add(nineButton);
+            Controls.Add(eightButton);
+            Controls.Add(sevenButton);
+            Controls.Add(multButton);
+            Controls.Add(sixButton);
+            Controls.Add(fiveButton);
+            Controls.Add(fourButton);
+            Controls.Add(subButton);
+            Controls.Add(threeButton);
+            Controls.Add(twoButton);
+            Controls.Add(oneButton);
+            Controls.Add(addButton);
+            Controls.Add(clearButton);
+            Controls.Add(pointButton);
+            Controls.Add(zeroButton);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button zeroButton;
+        private Button pointButton;
+        private Button clearButton;
+        private Button addButton;
+        private Button subButton;
+        private Button threeButton;
+        private Button twoButton;
+        private Button oneButton;
+        private Button multButton;
+        private Button sixButton;
+        private Button fiveButton;
+        private Button fourButton;
+        private Button divButton;
+        private Button nineButton;
+        private Button eightButton;
+        private Button sevenButton;
+        private Button equalMarkButton;
+        private RichTextBox inputRichTextBox;
+        private RichTextBox solveRichTextBox;
     }
 }
