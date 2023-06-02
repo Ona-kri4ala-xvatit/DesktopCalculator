3a825cd9a5278aefba5b04cac84eafa6d6f1dc56
Nijat Muradov <live4ever4@gmail.com>
2023-06-02 13:52:55 +0400

    Add code

--- DesktopCalculator/Form1.Designer.cs
+++ DesktopCalculator/Form1.Designer.cs
@@ -54,6 +54,7 @@
             zeroButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             zeroButton.Location = new Point(12, 307);
             zeroButton.Name = "zeroButton";
+            zeroButton.RightToLeft = RightToLeft.Yes;
             zeroButton.Size = new Size(59, 51);
             zeroButton.TabIndex = 0;
             zeroButton.Text = "0";
@@ -64,6 +65,7 @@
             pointButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             pointButton.Location = new Point(77, 307);
             pointButton.Name = "pointButton";
+            pointButton.RightToLeft = RightToLeft.Yes;
             pointButton.Size = new Size(59, 51);
             pointButton.TabIndex = 1;
             pointButton.Text = ".";
@@ -75,6 +77,7 @@
             clearButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             clearButton.Location = new Point(142, 307);
             clearButton.Name = "clearButton";
+            clearButton.RightToLeft = RightToLeft.Yes;
             clearButton.Size = new Size(59, 51);
             clearButton.TabIndex = 2;
             clearButton.Text = "C";
@@ -86,6 +89,7 @@
             addButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             addButton.Location = new Point(207, 307);
             addButton.Name = "addButton";
+            addButton.RightToLeft = RightToLeft.Yes;
             addButton.Size = new Size(59, 51);
             addButton.TabIndex = 3;
             addButton.Text = "+";
@@ -97,6 +101,7 @@
             subButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             subButton.Location = new Point(207, 250);
             subButton.Name = "subButton";
+            subButton.RightToLeft = RightToLeft.Yes;
             subButton.Size = new Size(59, 51);
             subButton.TabIndex = 7;
             subButton.Text = "-";
@@ -107,6 +112,7 @@
             threeButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             threeButton.Location = new Point(142, 250);
             threeButton.Name = "threeButton";
+            threeButton.RightToLeft = RightToLeft.Yes;
             threeButton.Size = new Size(59, 51);
             threeButton.TabIndex = 6;
             threeButton.Text = "3";
@@ -117,6 +123,7 @@
             twoButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             twoButton.Location = new Point(77, 250);
             twoButton.Name = "twoButton";
+            twoButton.RightToLeft = RightToLeft.Yes;
             twoButton.Size = new Size(59, 51);
             twoButton.TabIndex = 5;
             twoButton.Text = "2";
@@ -127,6 +134,7 @@
             oneButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             oneButton.Location = new Point(12, 250);
             oneButton.Name = "oneButton";
+            oneButton.RightToLeft = RightToLeft.Yes;
             oneButton.Size = new Size(59, 51);
             oneButton.TabIndex = 4;
             oneButton.Text = "1";
@@ -138,6 +146,7 @@
             multButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             multButton.Location = new Point(208, 193);
             multButton.Name = "multButton";
+            multButton.RightToLeft = RightToLeft.Yes;
             multButton.Size = new Size(59, 51);
             multButton.TabIndex = 11;
             multButton.Text = "*";
@@ -148,6 +157,7 @@
             sixButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             sixButton.Location = new Point(143, 193);
             sixButton.Name = "sixButton";
+            sixButton.RightToLeft = RightToLeft.Yes;
             sixButton.Size = new Size(59, 51);
             sixButton.TabIndex = 10;
             sixButton.Text = "6";
@@ -158,6 +168,7 @@
             fiveButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             fiveButton.Location = new Point(78, 193);
             fiveButton.Name = "fiveButton";
+            fiveButton.RightToLeft = RightToLeft.Yes;
             fiveButton.Size = new Size(59, 51);
             fiveButton.TabIndex = 9;
             fiveButton.Text = "5";
@@ -168,6 +179,7 @@
             fourButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             fourButton.Location = new Point(13, 193);
             fourButton.Name = "fourButton";
+            fourButton.RightToLeft = RightToLeft.Yes;
             fourButton.Size = new Size(59, 51);
             fourButton.TabIndex = 8;
             fourButton.Text = "4";
@@ -179,6 +191,7 @@
             divButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             divButton.Location = new Point(208, 136);
             divButton.Name = "divButton";
+            divButton.RightToLeft = RightToLeft.Yes;
             divButton.Size = new Size(59, 51);
             divButton.TabIndex = 15;
             divButton.Text = "/";
@@ -189,6 +202,7 @@
             nineButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             nineButton.Location = new Point(143, 136);
             nineButton.Name = "nineButton";
+            nineButton.RightToLeft = RightToLeft.Yes;
             nineButton.Size = new Size(59, 51);
             nineButton.TabIndex = 14;
             nineButton.Text = "9";
@@ -199,6 +213,7 @@
             eightButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             eightButton.Location = new Point(78, 136);
             eightButton.Name = "eightButton";
+            eightButton.RightToLeft = RightToLeft.Yes;
             eightButton.Size = new Size(59, 51);
             eightButton.TabIndex = 13;
             eightButton.Text = "8";
@@ -209,6 +224,7 @@
             sevenButton.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             sevenButton.Location = new Point(13, 136);
             sevenButton.Name = "sevenButton";
+            sevenButton.RightToLeft = RightToLeft.Yes;
             sevenButton.Size = new Size(59, 51);
             sevenButton.TabIndex = 12;
             sevenButton.Text = "7";
@@ -230,7 +246,7 @@
             inputRichTextBox.BorderStyle = BorderStyle.FixedSingle;
             inputRichTextBox.Enabled = false;
             inputRichTextBox.Font = new Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            inputRichTextBox.ForeColor = SystemColors.ControlText;
+            inputRichTextBox.ForeColor = Color.FromArgb(0, 0, 64);
             inputRichTextBox.Location = new Point(13, 44);
             inputRichTextBox.Name = "inputRichTextBox";
             inputRichTextBox.ReadOnly = true;
--- DesktopCalculator/Form1.cs
+++ DesktopCalculator/Form1.cs
@@ -5,6 +5,60 @@ namespace DesktopCalculator
         public Form1()
         {
             InitializeComponent();
+
+            #region Events
+            oneButton.Click += ButtonClick;
+            twoButton.Click += ButtonClick;
+            threeButton.Click += ButtonClick;
+            fourButton.Click += ButtonClick;
+            fiveButton.Click += ButtonClick;
+            sixButton.Click += ButtonClick;
+            sevenButton.Click += ButtonClick;
+            eightButton.Click += ButtonClick;
+            nineButton.Click += ButtonClick;
+            zeroButton.Click += ButtonClick;
+            pointButton.Click += ButtonClick;
+            clearButton.Click += ButtonClick;
+            addButton.Click += ButtonClick;
+            subButton.Click += ButtonClick;
+            multButton.Click += ButtonClick;
+            divButton.Click += ButtonClick;
+            #endregion
+        }
+
+        private void ButtonClick(object? sender, EventArgs e)
+        {
+            double firstNumber = 0;
+            double secondNumber = 0;
+
+            if (sender is Button button)
+            {
+                var inputText = button.Text; //Текст кнопки
+                string str = inputRichTextBox.Text; //строка ввода
+
+                if (inputText == "C")
+                {
+                    if (string.IsNullOrEmpty(str))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        inputRichTextBox.Text = str.Remove(str.Length - 1);
+                    }
+                }
+                else
+                {
+                    inputRichTextBox.Text += inputText;
+                    firstNumber = double.Parse(inputText);
+                }
+
+                if (inputText == "+")
+                {
+                    solveRichTextBox.Text += inputRichTextBox.Text;
+                    inputRichTextBox.Clear();
+                }
+            }
         }
     }
 }
